0b37d655ed0ec1cb24bd49eeb7d6944c96590479
EC2 Default User <ec2-user@ip-172-31-39-5.ec2.internal>
2020-11-06 15:26:25 +0000

    Added files

---
+++ main.py
@@ -0,0 +1,13 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Drone Project
+
+Authors: Blake Legge, Lyndon Loveys, Nicholas Hodder, Annette Clarke, Daniel Harris
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+#Imports
+
+#Functions
+
+#Program start
+if __name__ == "__main__":
+    print("Program starts here")
