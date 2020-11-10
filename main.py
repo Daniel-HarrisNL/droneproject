8bf2fb22b2debe608482863d8230ca316cb2e56b
Nicholas Hodder <74072942+Kirpeko@users.noreply.github.com>
2020-11-10 11:16:55 -0330

    Update main.py
    Created functioning main file to connect with other files and run program

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ Drone Project
 Authors: Blake Legge, Lyndon Loveys, Nicholas Hodder, Annette Clarke, Daniel Harris
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-#Imports
+# Imports
+import tello_abridged as ta
 
-#Functions
+# Functions
+'''
+DEFAULT_TELLO_COMMAND_IP - A constant representing the default IP address for commands on Tello per the documentation.
+'''
+DEFAULT_TELLO_COMMAND_IP = "192.168.10.1"
 
-#Program start
+'''
+DEFAULT_TELLO_COMMAND_PORT - A constant representing the default port for commands on Tello per the documentation.
+'''
+DEFAULT_TELLO_COMMAND_PORT = 8889
+
+t = ta.Tello()
+
+# Program start
 if __name__ == "__main__":
     print("Program starts here")
+    t.connect_and_initialize()
+    import gui
+    t.disconnect()
