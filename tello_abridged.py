57f3a28b8978fffc537a4fd687d78d69436e9346
Nicholas Hodder <74072942+Kirpeko@users.noreply.github.com>
2020-11-10 11:18:27 -0330

    Update tello_abridged.py
    Fixed some minor spelling mistakes/errors

--- tello_abridged.py
+++ tello_abridged.py
@@ -1,7 +1,7 @@
 '''
 TELLO_SIMPLE
     A module to help with controlling the Tello drones.
-    Written by Roy Brushett while recovering from sugery.
+    Written by Roy Brushett while recovering from surgery.
 '''
 import socket
 
@@ -15,10 +15,12 @@ DEFAULT_TELLO_COMMAND_PORT - A constant representing the default port for comman
 '''
 DEFAULT_TELLO_COMMAND_PORT = 8889
 
+
 class Tello:
     '''
     A class for connecting to the Tello Drone and controlling it with commands.
     '''
+
     def __init__(self, ip=DEFAULT_TELLO_COMMAND_IP, port=DEFAULT_TELLO_COMMAND_PORT):
         '''
         Constructor for the class. You can set the default ip and port here if you've changed them.
@@ -26,7 +28,7 @@ class Tello:
         import tello_simple
         t = tello_simple.Tello()
         '''
-        self.conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) 
+        self.conn = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.ip = ip
         self.port = port
 
@@ -41,12 +43,12 @@ class Tello:
         '''
         ip = self.ip if ip is None else ip
         port = self.port if port is None else port
-        self.conn.connect((ip,port))
-           
+        self.conn.connect((ip, port))
+
     def disconnect(self):
         '''
         Ends the connection to the drone over wifi.
-        Should be called at the end of your program or when you're othwerise finished sending commands.
+        Should be called at the end of your program or when you're otherwise finished sending commands.
         Returns nothing.
         '''
         self.conn.shutdown(socket.SHUT_RDWR)
@@ -55,9 +57,10 @@ class Tello:
     def connect_and_initialize(self, ip=None, port=None):
         '''
         Connects to the drone over wifi, and sends the initialization command to put it into command mode.
-        This allows it to recieve other commands, and only has to be done once.
+        This allows it to receive other commands, and only has to be done once.
 
-        This method returns the status message back from the drone (either an error or the string 'ok'), as well as the number of bytes sent.
+        This method returns the status message back from the drone (either an error or the string 'ok'), as well as the
+        number of bytes sent.
 
         Takes two optional parameters, the ip address of the drone, and the port of the drone.
         These can be left blank in most cases, it'd only change if you change the settings on the drone itself.
@@ -77,11 +80,11 @@ class Tello:
         Returns the response from the drone either the string 'ok' or an error.
         This method should not be used on it's own. Instead call "connect_and_initialize"
         '''
-        return send_initalize_command_mode_command_to_socket(self.conn)
+        return send_initialize_command_mode_command_to_socket(self.conn)
 
     def emergency(self):
         '''
-        Sends the emergecny command to the drone, forcing it to kill power to the motors immediately.
+        Sends the emergency command to the drone, forcing it to kill power to the motors immediately.
         Returns the response from the drone ('ok', or an error), as well as the number of bytes send.
         
         For example:
@@ -101,18 +104,19 @@ class Tello:
         '''
         return send_land_command_to_socket(self.conn)
 
-    def send_command(self,command_string):
+    def send_command(self, command_string):
         '''
         Sends a command string to the drone and returns it's response, as well as the number of bytes sent.
            command_string - A string representing a command, for example "takeoff"
            return - A tuple with two items:
-            Item 1: Whatever was recieved from the drone
+            Item 1: Whatever was received from the drone
             Item 2: The number of bytes sent.
         For example:
             t.send_command("takeoff")
             #returns: ('ok', 7) 
         '''
         return send_command_to_socket(self.conn, command_string)
+
 
 def send_command_to_socket(soc, command_string):
     '''
@@ -124,9 +128,12 @@ def send_command_to_socket(soc, command_string):
        return - ('ok' | error message, number_of_bytes)
     '''
     sent = soc.send(command_string.encode('utf-8'))
-    return (soc.recv(1024).decode('utf-8'),sent)
+    return (soc.recv(1024).decode('utf-8'), sent)
+
 
 LAND_COMMAND_STRING = "land"
+
+
 def send_land_command_to_socket(soc, send_command_to_socket=send_command_to_socket):
     '''
     This is a helper function that implements the behavior for a method in the Tello class.
@@ -138,20 +145,26 @@ def send_land_command_to_socket(soc, send_command_to_socket=send_command_to_sock
     '''
     return send_command_to_socket(soc, LAND_COMMAND_STRING)
 
+
 EMERGENCY_COMMAND_STRING = "emergency"
+
+
 def send_emergency_command_to_socket(soc, send_command_to_socket=send_command_to_socket):
     '''
     This is a helper function that implements the behavior for a method in the Tello class.
     This should not be used on it's own, use the Tello class instead.
-    Sends the emergecny command to the drone and returns it's response, as well as the number of bytes sent.
+    Sends the emergency command to the drone and returns it's response, as well as the number of bytes sent.
        soc - A socket like object that represents the connection to tello.
        send_command_to_socket (optional) - You can override the default send command if you'd like.
        return - ('ok' | error message, number_of_bytes)
     '''
     return send_command_to_socket(soc, EMERGENCY_COMMAND_STRING)
 
-INITALIZE_COMMAND_COMMAND_STRING = "command"
-def send_initalize_command_mode_command_to_socket(soc, send_command_to_socket=send_command_to_socket):
+
+INITIALIZE_COMMAND_COMMAND_STRING = "command"
+
+
+def send_initialize_command_mode_command_to_socket(soc, send_command_to_socket=send_command_to_socket):
     '''
     This is a helper function that implements the behavior for a method in the Tello class.
     This should not be used on it's own, use the Tello class instead.
@@ -160,4 +173,4 @@ def send_initalize_command_mode_command_to_socket(soc, send_command_to_socket=se
        send_command_to_socket (optional) - You can override the default send command if you'd like.
        return - ('ok' | error message, number_of_bytes)
     '''
-    return send_command_to_socket(soc, INITALIZE_COMMAND_COMMAND_STRING)
+    return send_command_to_socket(soc, INITIALIZE_COMMAND_COMMAND_STRING)
